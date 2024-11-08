34762686095e3b8ccd045569db70d1add520ffe2
pvinuale@emeal.nttdata.com <pvinuale@emeal.nttdata.com>
2024-11-08 09:00:06 +0000

    Modelos creados con lifetime value

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,11 +1,30 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
+with orders as  (
+    select * from {{ ref ('stg_orders' )}}
+),
 
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
+payments as (
+    select * from {{ ref ('stg_payments') }}
+),
 
-from raw.stripe.payment
+order_payments as (
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
+
+select * from final
